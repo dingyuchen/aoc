f1e68aca862a965caa0b2f676d6881cd371bb7d3
Ding YuChen <yuchen.ding@shopee.com>
2022-12-05 11:38:50 +0800

    Complete aoc 2022 day2

---
+++ aoc-2022/day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/Users/yuchen.ding/aoc/aoc-2022/day2/target/debug/day2: /Users/yuchen.ding/aoc/aoc-2022/day2/src/main.rs
---
+++ aoc-2022/day2/target/debug/deps/day2-7eac1ce21170c242.d
@@ -0,0 +1,5 @@
+/Users/yuchen.ding/aoc/aoc-2022/day2/target/debug/deps/day2-7eac1ce21170c242: src/main.rs
+
+/Users/yuchen.ding/aoc/aoc-2022/day2/target/debug/deps/day2-7eac1ce21170c242.d: src/main.rs
+
+src/main.rs:
